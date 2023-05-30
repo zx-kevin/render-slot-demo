854bb5dd74940edbe2edbf31b06529cf2b83b668
K <592031129@qq.com>
2023-05-30 22:15:31 +0800

    🧪 test: demo

---
+++ src/App.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="app-container">
+    <el-table :data="dataList">
+      <el-table-column label="test1" prop="test"></el-table-column>
+      <Column label="test2">
+        <template #default="scope">
+          <el-button type="primary" link @click="test(scope)">button</el-button>
+        </template>
+      </Column>
+    </el-table>
+  </div>
+</template>
+
+<script setup>
+import { ref } from 'vue'
+
+import Column from './components/column.vue'
+
+const dataList = ref([
+  {
+    test: 1
+  }
+])
+
+function test(scope) {
+  console.log('scope', scope)
+}
+</script>
+
+<style lang="scss" scoped></style>
---
+++ src/components/column.vue
@@ -0,0 +1,38 @@
+<template>
+  <el-table-column v-bind="$attrs">
+    <template #default="scope">
+      <slot v-bind="scope"></slot>
+
+      <renderComponent></renderComponent>
+    </template>
+  </el-table-column>
+</template>
+
+<script>
+export default {
+  name: 'TTableColumn'
+}
+</script>
+
+<script setup>
+import { ElTableColumn } from 'element-plus'
+import { useSlots, h, defineComponent, onMounted } from 'vue'
+
+const slots = useSlots()
+
+const renderComponent = defineComponent({
+  setup(props, { attrs }) {
+    return () =>
+      h(
+        'div',
+        slots.default().map((com) => com)
+      )
+  }
+})
+
+onMounted(() => {
+  console.log('1')
+})
+</script>
+
+<style lang="scss" scoped></style>
